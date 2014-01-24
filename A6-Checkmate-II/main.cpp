a4099f80914c7fc34cd9c0257f75ae03f6714ad5
Landon Epps <landon.epps@gmail.com>
2014-01-24 17:29:09 -0600

    Initial Commit

---
+++ A6-Checkmate-II/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  A6-Checkmate-II
+//
+//  Created by Landon Epps on 1/24/14.
+//  Copyright (c) 2014 Landon Epps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
